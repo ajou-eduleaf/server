ace32f55e695101fabb477267c41509602208aa5
sangjun <sangjun1389@ajou.ac.kr>
2023-11-22 17:20:03 +0900

    프로젝트 세팅

---
+++ src/test/java/com/eduleaf/DBproject/DBprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduleaf.DBproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DBprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
